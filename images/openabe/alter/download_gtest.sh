91793556f4ae24c5b07f9b8660bba557fa636932
NyuB <decaeste@etud.insa-toulouse.fr>
2021-10-27 08:24:02 +0200

    Docker image containing openABE binaries, libraries and development headers

---
+++ images/openabe/alter/download_gtest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+VERSION=$1
+if [[ $VERSION = "" ]]; then
+    echo "[!] Missing version as argument"
+    exit 0
+fi
+
+GTEST_VERSION=googletest-release-${VERSION}.zip
+#Corrected link
+GTEST_LINK=https://codeload.github.com/google/googletest/zip/refs/tags/release-${VERSION}
+
+if [[ ! -f ${GTEST_VERSION} ]]; then
+    wget -O ${GTEST_VERSION} ${GTEST_LINK}
+fi
